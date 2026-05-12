6470d41ec807f13748b8f4949b772a463b0a8057
Shubham Somani <smnshbhm@gmail.com>
2026-05-12 16:55:09 +0530

    Update greeting details for user Shubham Somani

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,10 +20,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Shubham Somani",
+  title: "Hi all, I'm Shubham",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "Data Analyst & Market Research Analyst 📊 skilled in Power BI, Excel, Dashboard Development, Healthcare Research and Market Analysis."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
